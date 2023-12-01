a533607f8faa507d19bd6616e74ee2258b66f3d3
Pythongitgoit <myemailforjearregistratioon@gmail.com>
2023-12-01 13:07:39 +0200

    Some bugs

--- Bot.py
+++ Bot.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 
 class UserInput(ABC):
     @abstractmethod
-    def Input(self):
+    def Input():
         pass
 
 
--- __main__.py
+++ __main__.py
@@ -1,24 +1,40 @@
-from Bot import Bot
+from Bot import ConsolBot
 
 
 if __name__ == "__main__":
-    print('Hello. I am your contact-assistant. What should I do with your contacts?')
-    bot = Bot()
+    print("Hello. I am your contact-assistant. What should I do with your contacts?")
+    # bot = Bot()
+    bot = ConsolBot()
+
     bot.book.load("auto_save")
-    commands = ['Add', 'Search', 'Edit', 'Load', 'Remove', 'Save', 'Congratulate', 'View', 'Exit']
+    commands = [
+        "Add",
+        "Search",
+        "Edit",
+        "Load",
+        "Remove",
+        "Save",
+        "Congratulate",
+        "View",
+        "Exit",
+    ]
     while True:
-        action = input('Type help for list of commands or enter your command\n').strip().lower()
-        if action == 'help':
-            format_str = str('{:%s%d}' % ('^',20))
+        action = (
+            input("Type help for list of commands or enter your command\n")
+            .strip()
+            .lower()
+        )
+        if action == "help":
+            format_str = str("{:%s%d}" % ("^", 20))
             for command in commands:
                 print(format_str.format(command))
             action = input().strip().lower()
             bot.handle(action)
-            if action in ['add', 'remove', 'edit']:
+            if action in ["add", "remove", "edit"]:
                 bot.book.save("auto_save")
         else:
             bot.handle(action)
-            if action in ['add', 'remove', 'edit']:
+            if action in ["add", "remove", "edit"]:
                 bot.book.save("auto_save")
-        if action == 'exit':
+        if action == "exit":
             break
